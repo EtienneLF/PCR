056ea63bf37b0f00625528f64791fa5a5b9bff61
Etienne Logeais <etienne.logeais@edu.esiee.fr>
2021-03-16 12:04:17 +0100

    Validation ok
    Co-authored-by: Charlotte Pook <Uncxnditixnally@users.noreply.github.com>

--- validation.c
+++ validation.c
@@ -46,14 +46,11 @@ int main(int argc, char* argv[])
     char* lignePCR = litLigne(df);
 
     while(strcmp(lignePCR, "erreur") != 0 ){
-        printf("Ligne lue: %s", lignePCR);
 
-        char resultat = lignePCR[strlen(lignePCR)-2]; //dernier caractère
+        char* resultat = &(lignePCR[strlen(lignePCR)-2]); //dernier caractère
+        int longueur = strlen(lignePCR);
+        char* timestamp = malloc(longueur-17-2); //tps validité (17-2 pour position initial - espace + resultat)
 
-        int longeur = strlen(lignePCR);
-
-        char* timestamp = malloc(longeur-17-2); //tps validité (17-2 pour position initial - espace + resultat)
-        
         for (int i =0; i < 16; i++){
             if (lignePCR[i] == emeteur[i]){ 
                 if( i == 15 ){
@@ -65,69 +62,27 @@ int main(int argc, char* argv[])
                         j = j+1;
                     }
 
-
-                    /**
-                     * 
-                     *  CODE INTERMEDIAIRE dsl c deg T-T
-                     * 
-                    */
-                   
-                    //Recuperation de la date du jour (date de la demande)
-                    time_t timestamp1 = time( NULL );
-                    struct tm * pTime = localtime( & timestamp1 );
-
-                    //char buffer[ MAX_SIZE ];
-                    //strftime( buffer, MAX_SIZE, "%Y%m%d %H:%M:%S", pTime );
-                    //printf( "Time : %s\n", buffer ); //à supprimer
+                    time_t now;
+                    now = time(NULL);
                     
 
-                    //test calcul jour deg faire switch ds le while
-                    char jourTest[2];
-                    jourTest[0] = timestamp[0];
-                    jourTest[1] = timestamp[1];
-                    int jourTestint = atoi(jourTest);
-                    printf("jour du Test: %d \n", jourTestint);
-                    int calculJour = pTime->tm_mday - jourTestint;
-                    printf("calcul du jour : %d \n", calculJour);
-
-                    char moisTest[2];
-                    moisTest[0] = timestamp[2];
-                    moisTest[1] = timestamp[3];
-                    int moisTestint = atoi(moisTest);
-                    printf("mois du Test: %d \n", moisTestint);
-                    int calculMois = ((pTime->tm_mon+1) - moisTestint)*30; //*30 pour avoir le nbr de mois en jour
-                    printf("calcul du mois : %d \n", calculMois);
-
-                    char anneeTest[4];
-                    anneeTest[0] = timestamp[4];
-                    anneeTest[1] = timestamp[5];
-                    anneeTest[2] = timestamp[6];
-                    anneeTest[3] = timestamp[7];
-                    int anneeTestint = atoi(anneeTest);
-                    printf("annee du Test: %d \n", anneeTestint);
-                    int calculAnnee = ((pTime->tm_year+1900) - anneeTestint)*360; //$360 pour avoir le nbr d'année en jours
-                    printf("calcul de l'année : %d \n", calculAnnee); //ok
-
-                    //Comparaison durée validité et durée entre la réalisation du test et aujourd'hui
-                    int calculFinal = calculJour + calculMois + calculAnnee;
-                    int valeurTest = atoi(valeur);
-                    printf("Durée (en jours) entre la date du test et la date actuelle: %d \n", calculFinal);
-                    if((-calculFinal) <= valeurTest){ //-CalculFinal car si on envoie une demande dont le test a été fait plus tard la valeur finale va être négative
-                        printf("Validité du test correct \n");
+                    char* valide = "0";
+                    int iValeur = atoi(valeur);
+                    int iTimestamp = atoi(timestamp);
+                    
+                    if (now <= (iTimestamp + iValeur)){  
+                        if(strcmp(resultat,"1\n") == 0){
+                            valide = "1";
+                        }
                     }
-                    else{
-                        printf("Validité du test incorrect \n");
-                    }
-
-                    printf("timestamp: %s \n", timestamp);
-                    printf("resultat : %c \n", resultat);
-
+                    char *msg = message(emeteur,"Reponse", valide);
+                    ecritLigne(1,msg);
 
                     free(timestamp);
                 }
             } 
             else{
-                printf("Numéro du test inconnu. \n");
+                //printf("Numéro du test inconnu. \n");
                 break;
             }
             
@@ -137,7 +92,6 @@ int main(int argc, char* argv[])
 
         /*if(strcmp(emeteur,emeteurPCR) == 0){
             |0001000000000000|Demande|13021|
-            |0001000000000000|Demande|180| --> durée de validité de 180 jours
         }*/
 
         lignePCR = litLigne(df);
